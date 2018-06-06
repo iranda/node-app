99057059f4372febcc17473b185520697c9d619f
Daria Sirous <daria.sirous@gmail.com>
2018-06-06 17:53:04 -0400

    Updates pipeline

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send('Hello world. It\'s working');
 });
 
 export default router;
